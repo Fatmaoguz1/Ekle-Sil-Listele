fe70151a2b259cb1aa37d1da7ce42e88b9e07565
Fatmaoguz1 <122368817+Fatmaoguz1@users.noreply.github.com>
2023-03-27 01:28:06 +0300

    Add files via upload

---
+++ ekle-sil-listele/ödev1/Form2.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ödev1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxadres = new System.Windows.Forms.TextBox();
+            this.textBoxtel = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.ticaret1DataSet = new ödev1.ticaret1DataSet();
+            this.müşteriBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.müşteriTableAdapter = new ödev1.ticaret1DataSetTableAdapters.müşteriTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adresDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticaret1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.müşteriBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(48, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(39, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adres";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(39, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tel";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 33);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBoxadres
+            // 
+            this.textBoxadres.Location = new System.Drawing.Point(136, 106);
+            this.textBoxadres.Multiline = true;
+            this.textBoxadres.Name = "textBoxadres";
+            this.textBoxadres.Size = new System.Drawing.Size(268, 33);
+            this.textBoxadres.TabIndex = 5;
+            // 
+            // textBoxtel
+            // 
+            this.textBoxtel.Location = new System.Drawing.Point(136, 172);
+            this.textBoxtel.Multiline = true;
+            this.textBoxtel.Name = "textBoxtel";
+            this.textBoxtel.Size = new System.Drawing.Size(268, 33);
+            this.textBoxtel.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.adresDataGridViewTextBoxColumn,
+            this.telDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.müşteriBindingSource;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 283);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1304, 301);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(452, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 56);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(452, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 56);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(452, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 56);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ticaret1DataSet
+            // 
+            this.ticaret1DataSet.DataSetName = "ticaret1DataSet";
+            this.ticaret1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // müşteriBindingSource
+            // 
+            this.müşteriBindingSource.DataMember = "müşteri";
+            this.müşteriBindingSource.DataSource = this.ticaret1DataSet;
+            // 
+            // müşteriTableAdapter
+            // 
+            this.müşteriTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // adresDataGridViewTextBoxColumn
+            // 
+            this.adresDataGridViewTextBoxColumn.DataPropertyName = "Adres";
+            this.adresDataGridViewTextBoxColumn.HeaderText = "Adres";
+            this.adresDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.adresDataGridViewTextBoxColumn.Name = "adresDataGridViewTextBoxColumn";
+            // 
+            // telDataGridViewTextBoxColumn
+            // 
+            this.telDataGridViewTextBoxColumn.DataPropertyName = "Tel";
+            this.telDataGridViewTextBoxColumn.HeaderText = "Tel";
+            this.telDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.telDataGridViewTextBoxColumn.Name = "telDataGridViewTextBoxColumn";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(687, 48);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 46);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Listele";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(1008, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(191, 50);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Temizle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(1328, 596);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBoxtel);
+            this.Controls.Add(this.textBoxadres);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticaret1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.müşteriBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxadres;
+        private System.Windows.Forms.TextBox textBoxtel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private ticaret1DataSet ticaret1DataSet;
+        private System.Windows.Forms.BindingSource müşteriBindingSource;
+        private ticaret1DataSetTableAdapters.müşteriTableAdapter müşteriTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adresDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ ekle-sil-listele/ödev1/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ödev1
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti =new SqlConnection("Data Source=DESKTOP-K6LN5D6\\SQLEXPRESS;Initial Catalog=ticaret1;Integrated Security=True");
+        
+        void temizle()
+        {
+            textBoxadres.Text = "";
+            textBoxtel.Text = "";
+            textBox1.Text = "";
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'ticaret1DataSet.müşteri' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.müşteriTableAdapter.Fill(this.ticaret1DataSet.müşteri);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            //textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            //textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            //textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+
+            SqlCommand komut =new SqlCommand("insert into müşteri (Adres,Tel) values (@adres,@tel)",baglanti);
+            komut.Parameters.AddWithValue("@adres", textBoxadres.Text);
+            komut.Parameters.AddWithValue("@tel", textBoxtel.Text);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("müsteri eklendi.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand sil =new SqlCommand("Delete From müşteri where id=@p1",baglanti);
+            sil.Parameters.AddWithValue("@p1", textBox1.Text);
+            sil.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Kayıt Silindi.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand guncelle = new SqlCommand("Update müşteri Set Adres=@p1,Tel=@p2 where id=@p3",baglanti);
+            guncelle.Parameters.AddWithValue("p1",textBoxadres.Text);
+            guncelle.Parameters.AddWithValue("p2", textBoxtel.Text);
+            guncelle.Parameters.AddWithValue("p3", textBox1.Text);
+            guncelle.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("güncelleme basarılı.");
+        }
+
+       
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.müşteriTableAdapter.Fill(this.ticaret1DataSet.müşteri);
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secim = dataGridView1.SelectedCells[0].RowIndex;
+            textBox1.Text= dataGridView1.Rows[secim].Cells[0].Value.ToString();
+            textBoxadres.Text = dataGridView1.Rows[secim].Cells[1].Value.ToString();
+            textBoxtel.Text = dataGridView1.Rows[secim].Cells[2].Value.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+    }
+}
